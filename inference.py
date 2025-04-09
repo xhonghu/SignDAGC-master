5baad5f0a5a991b48e74bddd6db6698b9f906b38
Wild geese soaring by the lakeside <103254694+xhonghu@users.noreply.github.com>
2025-04-09 19:38:02 +0800

    Update inference.py

--- inference.py
+++ inference.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import glob
 import cv2
+import yaml
 from utils import video_augmentation
 from Tokenizer import GlossTokenizer_S2G
 from slr_network import SLRModel
@@ -9,24 +10,37 @@ import torch
 from collections import OrderedDict
 import utils
 from utils.metrics import wer_list
-    
+
+
+###
 gpu_id = 0 # The GPU to use
 dataset = 'phoenix2014-T'  # support [phoenix2014, phoenix2014-T, CSL-Daily]
-prefix = '/new_home/PHOENIX-2014-T-release-v3/PHOENIX-2014-T/'
-gloss_tokenizer = {}
-gloss_tokenizer['gloss2id_file'] = f'./preprocess/{dataset}/gloss2ids.pkl'
-if 'phoenix' in dataset:
-    gloss_tokenizer['lower_case'] = False
-
-model_weights = f'./{dataset}/_best_model.pt'
-
-select_id = 99 
-
+select_id = 12
+### 
+sparser = utils.get_parser()
+p = sparser.parse_args()
+if p.config is not None:
+    with open(p.config, 'r') as f:
+        try:
+            default_arg = yaml.load(f, Loader=yaml.FullLoader)
+        except AttributeError:
+            default_arg = yaml.load(f)
+    key = vars(p).keys()
+    for k in default_arg.keys():
+        if k not in key:
+            print('WRONG ARG: {}'.format(k))
+            assert (k in key)
+    sparser.set_defaults(**default_arg)
+args = sparser.parse_args()
+with open(f"./configs/{dataset}.yaml", 'r') as f:
+    args.dataset_info = yaml.load(f, Loader=yaml.FullLoader)
+model_weights = f'./{dataset}/best_model.pt'
+prefix = args.dataset_info['dataset_root']
 # Load data and apply transformation
-gloss_tokenizer = GlossTokenizer_S2G(gloss_tokenizer)
+gloss_tokenizer = GlossTokenizer_S2G(args.dataset_info['gloss'])
 inputs_list = np.load(f"./preprocess/{dataset}/dev_info.npy", allow_pickle=True).item()
 name = inputs_list[select_id]['fileid']
-img_folder = os.path.join(prefix, "features/fullFrame-210x260px/" + inputs_list[select_id]['folder']) if 'phoenix' in dataset else os.path.join(prefix, inputs_list[select_id]['folder'])
+img_folder = os.path.join(prefix, "features/fullFrame-210x260px/" + inputs_list[select_id]['folder']) if 'phoenix' in dataset else os.path.join(prefix, "sentence/frames_512x512/" +inputs_list[select_id]['folder'])
 img_list = sorted(glob.glob(img_folder))
 img_list = [cv2.cvtColor(cv2.resize(cv2.imread(img_path), (256, 256), interpolation=cv2.INTER_LANCZOS4), cv2.COLOR_BGR2RGB) for img_path in img_list]
 label_list = gloss_tokenizer(inputs_list[select_id]['label'].split(" "))
@@ -38,7 +52,6 @@ transform = video_augmentation.Compose([
 vid = transform(img_list)
 vid = vid.float() / 127.5 - 1
 vid = vid.unsqueeze(0)
-
 left_pad = 0
 last_stride = 1
 total_stride = 1
@@ -61,6 +74,7 @@ vid = torch.cat(
         vid[0,-1][None].expand(max_len - vid.size(1) - left_pad, -1, -1, -1),
     )
     , dim=0).unsqueeze(0)
+#load model
 device = utils.GpuDataParallel()
 device.set_device(gpu_id)
 model = SLRModel(num_classes=len(gloss_tokenizer), c2d_type='resnet34', conv_type=2, use_bn=1, gloss_dict=gloss_tokenizer)
@@ -76,6 +90,7 @@ model.load_state_dict(s_dict, strict=True)
 model = model.to(device.output_device)
 model.cuda()
 model.eval()
+#inference
 vid = device.data_to_device(vid)
 vid_lgt = device.data_to_device(video_length)
 label = device.data_to_device([torch.LongTensor(label_list['gloss_labels'])])
